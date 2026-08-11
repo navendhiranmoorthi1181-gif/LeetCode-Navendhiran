1905cc29a8d064e0e04b6c00caad7144da709c50
navendhiranmoorthi1181-gif <navendhiranmoorthi1181@gmail.com>
2026-08-11 14:18:20 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,23 @@
+// Last updated: 8/11/2026, 2:18:19 PM
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0, j = 0, count = 0;
+
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                count++;
+                i++;
+                j++;
+            } else if (g[i] > s[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return count;
+    }
+}
