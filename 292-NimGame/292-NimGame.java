a2dc1243f2930868464c3747ef65644e15f0bedb
navendhiranmoorthi1181-gif <navendhiranmoorthi1181@gmail.com>
2026-08-11 14:19:20 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 8/11/2026, 2:19:19 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        
+        if(n%4==0)
+        return false;
+        else
+        return true;
+    
+    }
+}
