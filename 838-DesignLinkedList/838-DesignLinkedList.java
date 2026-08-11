0c9083e84291f630635b0258aaf1735f1e90ca78
navendhiranmoorthi1181-gif <navendhiranmoorthi1181@gmail.com>
2026-08-11 14:16:59 +0530

    Create 838-DesignLinkedList

---
+++ 838-DesignLinkedList/838-DesignLinkedList.java
@@ -0,0 +1,74 @@
+// Last updated: 8/11/2026, 2:16:59 PM
+class MyLinkedList {
+    // Node definition
+    private class Node {
+        int val;
+        Node next;
+        Node(int val) { this.val = val; }
+    }
+    
+    private Node head;   // start of the list
+    private int size;    // track length
+    
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+    
+    // Get value at index
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node curr = head;
+        for (int i = 0; i < index; i++) curr = curr.next;
+        return curr.val;
+    }
+    
+    // Add at head
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+    
+    // Add at tail
+    public void addAtTail(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node curr = head;
+            while (curr.next != null) curr = curr.next;
+            curr.next = newNode;
+        }
+        size++;
+    }
+    
+    // Add at index
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        Node newNode = new Node(val);
+        Node curr = head;
+        for (int i = 0; i < index - 1; i++) curr = curr.next;
+        newNode.next = curr.next;
+        curr.next = newNode;
+        size++;
+    }
+    
+    // Delete at index
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node curr = head;
+            for (int i = 0; i < index - 1; i++) curr = curr.next;
+            curr.next = curr.next.next;
+        }
+        size--;
+    }
+}
