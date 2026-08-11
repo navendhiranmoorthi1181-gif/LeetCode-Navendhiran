2a4f2fe2e0e3dc968f04bcc906ad97026d80fa37
navendhiranmoorthi1181-gif <navendhiranmoorthi1181@gmail.com>
2026-08-11 14:16:08 +0530

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 2:16:08 PM
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0)
+     return 0;
+     if(n==1||n==2)
+     return 1;
+     int n1=0,n2=1,n3=1,ans=0;
+     for(int i=3;i<=n;i++)
+     {
+        ans=n1+n2+n3;
+        n1=n2;
+        n2=n3;
+        n3=ans;
+     }
+     return n3;
+    }
+}
