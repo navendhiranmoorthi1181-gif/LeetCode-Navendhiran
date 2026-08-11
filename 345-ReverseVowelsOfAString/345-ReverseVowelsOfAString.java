ff51c1600aa4e8ab6dcb9348b5748c7ae642b41c
navendhiranmoorthi1181-gif <navendhiranmoorthi1181@gmail.com>
2026-08-11 14:19:08 +0530

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.java
@@ -0,0 +1,22 @@
+// Last updated: 8/11/2026, 2:19:07 PM
+class Solution {
+    public String reverseVowels(String s) {
+        char[] word = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        String vowels = "aeiouAEIOU";
+        while (start < end) {
+            while (start < end && vowels.indexOf(word[start]) == -1)
+                start++;
+            while (start < end && vowels.indexOf(word[end]) == -1)
+                end--;
+            char temp = word[start];
+            word[start] = word[end];
+            word[end] = temp;
+            start++;
+            end--;
+        }
+        String answer = new String(word);
+        return answer;
+    }
+}
