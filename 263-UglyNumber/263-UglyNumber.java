0411c91fb04d33349e3b8602e1f670dd57297ecb
navendhiranmoorthi1181-gif <navendhiranmoorthi1181@gmail.com>
2026-08-11 14:19:35 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,28 @@
+// Last updated: 8/11/2026, 2:19:34 PM
+
+class Solution {
+    public boolean isUgly(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n > 1) {
+
+            if (n % 2 == 0) {
+                n /= 2;
+            } 
+            else if (n % 3 == 0) {
+                n /= 3;
+            } 
+            else if (n % 5 == 0) {
+                n /= 5;
+            } 
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
