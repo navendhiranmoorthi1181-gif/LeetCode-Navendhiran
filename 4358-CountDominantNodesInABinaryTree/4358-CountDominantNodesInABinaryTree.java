a416c766aa8d7afcc44f5527b17346c1fc2e4e29
navendhiranmoorthi1181-gif <navendhiranmoorthi1181@gmail.com>
2026-08-11 14:13:39 +0530

    Create 4358-CountDominantNodesInABinaryTree

---
+++ 4358-CountDominantNodesInABinaryTree/4358-CountDominantNodesInABinaryTree.java
@@ -0,0 +1,37 @@
+// Last updated: 8/11/2026, 2:13:39 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    public int countDominantNodes(TreeNode root) {
+        dfs(root);
+        return count;
+    }
+    private int dfs(TreeNode node){
+        if(node==null){
+            return Integer.MIN_VALUE;
+        }
+        int leftMax=dfs(node.left);
+        int rightMax=dfs(node.right);
+
+        int subTreeMax=Math.max(node.val,Math.max(leftMax,rightMax));
+
+        if(node.val==subTreeMax){
+            count++;
+        }
+        return subTreeMax;
+    }
+}
